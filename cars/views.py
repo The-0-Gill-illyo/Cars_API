7989dadc8b58af371bc5f94a1103d3032e8af203
Howard Gill <theogill1369@gmail.com>
2022-08-24 12:00:37 -0400

    Modified cars/urls.py + cars/views.pf files to add Get_Car_Id in postman, currently throwinf 404 error, attempting to fix

--- cars/views.py
+++ cars/views.py
@@ -11,12 +11,14 @@ def cars_list(request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
-
-
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    
+@api_view(['GET'])
+def car_detail(request, pk):
+
+    print(pk)
+    return Response(pk)
